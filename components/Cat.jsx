ec7b0d778163ba2158d31f4198155570277c6c53
Marina Batina <mbatina.oat@gmail.com>
2023-08-21 16:07:00 +0300

    Use global CSS for building a cat figure instead of a separate module (to ensure Render build is okay).

--- components/Cat.jsx
+++ components/Cat.jsx
@@ -1,6 +1,5 @@
 "use client";
 
-// import classes from '../styles/Cat.module.css';
 import { useState } from 'react';
 
 const Cat =({onFeed})=>{
@@ -12,23 +11,30 @@ const Cat =({onFeed})=>{
         onFeed(isHunger);
     }
 
+
     return (
         <>
-        {isAwake && <div className={classes.hunger}>Hunger</div>}
-        <div className={classes.container} onClick={wakeUpHandler}>
-            <div className={!isAwake && classes.shadow}></div>
-            <div>instead of a cat</div>
+        {isAwake && <div className="hunger">
+            <div className="h">H</div>
+            <div className="u">U</div>
+            <div className="n">N</div>
+            <div className="g">G</div>
+            <div className="e">E</div>
+            <div className="r">R</div>
+            </div>}
+        <div className="container" onClick={wakeUpHandler}>
+            <div className={!isAwake ? "hadow":''}></div>
 
-            <div className={classes.cat}>
-                <div className={classes.ear}></div>
-                <div className={!isAwake? classes.eye:classes.eyeOpen}></div>
-                <div className={classes.mouth}></div>
-                <div className={classes.nose}></div>
-                <div className={!isAwake? classes.tailAction:classes.tailUpDown}></div>
-                <div className={!isAwake? classes.body: classes.bodyAwake}></div>
-                <div className={classes.bubble}></div>
-                <div className={classes.bubbleWithText}>
-                    <p className={classes.text}>{!isAwake? "Wake Me Up":"Feed Me"}</p>
+            <div className="cat">
+                <div className="ear"></div>
+                <div className={!isAwake? "eye":"eyeOpen"}></div>
+                <div className="mouth"></div>
+                <div className= "nose"></div>
+                <div className={!isAwake? "tailAction":"tailUpDown"}></div>
+                <div className={!isAwake? "body": "bodyAwake"}></div>
+                <div className="bubble"></div>
+                <div className="bubbleWithText">
+                    <p className="text">{!isAwake? "Wake Me Up":"Feed Me"}</p>
                     </div>
             </div>
         </div>
