eb794a005d18ff637e84674f5946f488fa8b2d1b
Marina Batina <mbatina.oat@gmail.com>
2023-08-25 15:44:43 +0300

    [Experiments] Temporarily hard code Mongo URI in attempt to fix Render.com deployment issue.

--- utils/database.js
+++ utils/database.js
@@ -9,9 +9,12 @@ if(isConnected) {
     console.log('MongoDB is already connected');
     return;
   }
- 
+  const MONGODB_URI = 'mongodb+srv://marina:SS0uiOo8qkJANgc2@marinacluster.nyhy8c8.mongodb.net/?retryWrites=true&w=majority';
   try {
-    await mongoose.connect(process.env.MONGODB_URI, {
+    await mongoose.connect(
+      MONGODB_URI
+      //process.env.MONGODB_URI
+      , {
       dbName: "personal_dictionary",
       useNewUrlParser: true,
       useUnifiedTopology: true,
